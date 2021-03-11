24fa366e9835f8f175e117fcf1df08dbdd980759
darknoob1971 <abdullah149161@bsdi-bd.org>
2021-03-11 19:25:38 +0600

    fix static file error

--- Real_Estate_Website/settings.py
+++ Real_Estate_Website/settings.py
@@ -137,13 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storge.CompressedManifestStaticFilesStorage'
-
 
 # Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
